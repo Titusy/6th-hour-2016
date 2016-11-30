bededc28f449a38dddfed1ada7dad8d39f4d2e32
TItus Yang <titusyang@claytonschools.net>
2016-11-30 12:22:29 -0600

    f
    fsda

--- team6.py
+++ team6.py
@@ -7,8 +7,9 @@
 ####
 
 team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'T' # Only 10 chars displayed.
+strategy_name = 'Arnold'
+strategy_description = 'Betrtay every single time'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,7 +27,14 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if my_history=='':
+        return 'b'
+    elif my_score<their_score:
+        return 'b'
+    elif their_history[-1]=='b':
+        return 'b'
+    else:
+        return 'b'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
